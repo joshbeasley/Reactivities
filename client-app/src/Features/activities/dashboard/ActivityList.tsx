c53a5effbf50a46eb9ef01e71b4c44bb1d99585c
Josh Beasley <josh.beasley@hotmail.com>
2020-08-12 17:53:00 -0400

    End of Section 5

---
+++ client-app/src/Features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Item, Image, Button, Label, Segment } from "semantic-ui-react";
+import { IActivity } from "../../../App/models/activity";
+import { act } from "react-dom/test-utils";
+
+interface IProps {
+	activities: IActivity[];
+	selectActivity: (id: string) => void;
+	deleteActivity: (id: string) => void;
+}
+
+export const ActivityList: React.FC<IProps> = ({
+	activities,
+	selectActivity,
+	deleteActivity,
+}) => {
+	return (
+		<Segment clearing>
+			<Item.Group divided>
+				{activities.map((activity) => (
+					<Item key={activity.id}>
+						<Item.Content>
+							<Item.Header as="a">{activity.title}</Item.Header>
+							<Item.Meta>{activity.date}</Item.Meta>
+							<Item.Description>
+								<div>{activity.description}</div>
+								<div>
+									{activity.city}, {activity.venue}
+								</div>
+							</Item.Description>
+							<Item.Extra>
+								<Button
+									floated="right"
+									content="View"
+									color="blue"
+									onClick={() => selectActivity(activity.id)}
+								/>
+								<Button
+									floated="right"
+									content="Delete"
+									color="red"
+									onClick={() => deleteActivity(activity.id)}
+								/>
+								<Label basic content={activity.category} />
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				))}
+			</Item.Group>
+		</Segment>
+	);
+};
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./App/layout/styles.css";
+import App from "./App/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 
